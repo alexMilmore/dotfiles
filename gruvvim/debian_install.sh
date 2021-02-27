b1e6f88c9f517004274126a06a0dae18c025a637
Alex Milmore <alex.milmore@unipart.io>
2021-02-27 13:30:35 +0000

    Started project

---
+++ gruvvim/debian_install.sh
@@ -0,0 +1,45 @@
+apt update
+
+##############
+# ESSENTIALS #
+##############
+
+# terminal emulator
+apt install kitty
+
+# text editor
+apt install neovim
+
+# terminal multiplexor
+apt install tmux
+
+# window manager
+apt install i3
+
+# web browser
+apt install firefox
+
+# shell
+apt install zsh
+
+#########################
+# PROGRAMMING LANGUAGES #
+#########################
+
+# rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+# haskell
+apt install haskell-platform
+
+
+###################
+# QUALITY OF LIFE #
+###################
+
+#######
+# FUN #
+#######
+
+# better looking top command
+apt install htop
