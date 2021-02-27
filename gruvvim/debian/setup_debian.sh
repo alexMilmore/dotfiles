cb052da625f39b83ad0651a7e456010e1b003992
Alex Milmore <alex.milmore@unipart.io>
2021-02-27 22:58:02 +0000

    remove old symlinks before adding

--- gruvvim/debian/setup_debian.sh
+++ gruvvim/debian/setup_debian.sh
@@ -8,10 +8,13 @@ nvim +'PlugInstall --sync' +qa
 
 ### symlings for config files
 # bash
+rm ~/.bashrc
 ln -s ~/.config/bash/bashrc ~/.bashrc
 
 # zshell
+rm ~/.zshrc
 ln -s ~/.config/zsh/zshrc ~/.zshrc
 
 # tmux
+rm ~/.tmux.conf
 ln -s ~/.config/tmux.conf ~/.tmux.conf
