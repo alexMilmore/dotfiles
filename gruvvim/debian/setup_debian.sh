f5066280a436e025f7857ab8532e579d0c0df1a1
Alex Milmore <alex.milmore@unipart.io>
2021-02-27 18:54:26 +0000

    symlinks for dotfiles not in .confing

--- gruvvim/debian/setup_debian.sh
+++ gruvvim/debian/setup_debian.sh
@@ -5,3 +5,11 @@ cp -r ../config/. ~/.config/.
 
 # install neovim plugins
 nvim +'PlugInstall --sync' +qa
+
+### symlings for config files
+# zshell
+ln -s ~/.config/zsh/.zshrc ~/.zshrc
+ln -s ~/.config/zsh/.zshenv ~/.zshenv
+
+# tmux
+ln -s ~/.config/.tmux.conf ~/.tmux.conf
