9cfd0e79d3beaa345580e6fa8896da61dfdbccf4
Alex Milmore <alexmilmore0@gmail.com>
2021-08-22 16:54:09 +0100

    more useful tools in install

--- gruvvim/install.sh
+++ gruvvim/install.sh
@@ -155,7 +155,10 @@ echo \
   "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt update
-apt install docker-ce docker-ce-cli containerd.io
+apt install -y docker-ce docker-ce-cli containerd.io docker-compose
+
+# wireshark
+apt install -y wireshark
 
 
 ###########################################################
