d49afb2ae0fbd435bc68442911aa1a0c9d2060d5
Alex Milmore <alexmilmore0@gmail.com>
2022-01-14 15:48:55 +0000

    ask if kmonad is installed

--- gruvvim/set_up_services.sh
+++ gruvvim/set_up_services.sh
@@ -8,6 +8,18 @@ if [[ $EUID -ne 0 ]]; then
 	exit 1
 fi
 
+echo "have you built kmonad and placed the executable in /usr/sbin/kmonad"
+read -r -p "have you built kmonad and placed the executable in /usr/sbin/kmonad [y/N] " response
+case "$response" in
+    [yY][eE][sS]|[yY])
+        echo "good, creating service"
+        ;;
+    *)
+        echo "please do that 1st"
+        exit 1
+        ;;
+esac
+
 # copy .service files
 cp ./config/kmonad/kmonad.service /etc/systemd/system/kmonad.service
 cp ./config/kmonad/alex_thinkpad.kbd /etc/systemd/kmonad.conf
